e86240f4219f998f948746cceb6df0e288c6cfcd
Daloar Hossan <daloarhossan128@gmail.com>
2022-04-29 10:54:47 +0600

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './Pages/Shared/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
